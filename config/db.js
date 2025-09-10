9d369c44679f1421140d42d85ffcf58b62ea04d4
armitaahmdi <armitaahamdi.business@gmail.com>
2025-09-10 19:04:11 +0330

    sqlite3

--- config/db.js
+++ config/db.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  storage: process.env.DB_NAME, // SQLite database file path
+  logging: false // Disable logging for cleaner output
+});
 
 module.exports = sequelize;

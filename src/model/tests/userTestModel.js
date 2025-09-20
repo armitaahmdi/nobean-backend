69e1e5b88860954d4a8af8fd8e47378eb80f2ed4
armitaahmdi <armitaahamdi.business@gmail.com>
2025-09-20 16:34:46 +0330

    usertestModel

--- src/model/tests/userTestModel.js
+++ src/model/tests/userTestModel.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     id:{
           type: DataTypes.BIGINT,
              primaryKey:true,
-          
+          autoIncrement: true
     },
     role: {
       type: DataTypes.STRING, // مثلاً "participant" یا "observer"
@@ -20,7 +20,44 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey:false
 
     },
-
+    // فیلدهای جدید برای ذخیره نتیجه آزمون
+    answers: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      comment: 'پاسخ‌های کاربر به صورت JSON'
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: 'نمره کاربر (0-100)'
+    },
+    correctAnswers: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: 'تعداد پاسخ‌های صحیح'
+    },
+    totalQuestions: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: 'تعداد کل سوالات'
+    },
+    timeSpent: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+      comment: 'زمان صرف شده به ثانیه'
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: 'in_progress',
+      comment: 'وضعیت آزمون: in_progress, completed'
+    },
+    completedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      comment: 'زمان تکمیل آزمون'
+    }
 
   });
 
